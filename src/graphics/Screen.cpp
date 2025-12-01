f5bf6b090f7664aaf9231fe5df120fe94911c249
R Talley <rtalleyhcso@gmail.com>
2025-12-01 17:24:51 -0500

    Guard digital clock battery UI

--- src/graphics/Screen.cpp
+++ src/graphics/Screen.cpp
@@ -638,11 +638,13 @@ void Screen::drawBatteryMeterButton(OLEDDisplay *display, int16_t x, int16_t y, 
 // Draw a digital clock
 void Screen::drawDigitalClockFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y)
 {
+    Screen *self = static_cast<Screen *>(state->userData);
+
     display->setTextAlignment(TEXT_ALIGN_LEFT);
 
     drawBattery(display, x, y + 7, imgBattery, powerStatus);
 
-    if (battMeterActive) {
+    if (self && self->battMeterActive) {
         if (powerStatus->getHasBattery()) {
             String batteryPercent = String(powerStatus->getBatteryChargePercent()) + "%";
             display->setFont(FONT_SMALL);
